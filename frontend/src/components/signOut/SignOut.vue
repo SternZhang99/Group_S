9e5fc66e0f824a1d35f7214f748d54a126097f72
iamzmlin <113278086+iamzmlin@users.noreply.github.com>
2023-06-07 13:38:33 +0800

    Create SignOut.vue

---
+++ frontend/src/components/signOut/SignOut.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>signOut success</h1>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+    export default {
+        mounted() {
+            Vue.prototype.$jwt=undefined
+            this.$router.push('/home')
+        },
+    }
+</script>
