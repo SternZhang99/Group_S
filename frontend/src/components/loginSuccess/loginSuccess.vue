ef3443f5e38515400fa61b0c2aa512513e82f957
iamzmlin <113278086+iamzmlin@users.noreply.github.com>
2023-06-07 13:37:07 +0800

    Create loginSuccess.vue
    submit the loginSuccess.vue

---
+++ frontend/src/components/loginSuccess/loginSuccess.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>login success</h1>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+    export default {
+        mounted() {
+            let jwt = window.location.href.split('id_token=')[1].split('&')[0]
+            Vue.prototype.$jwt=jwt
+            this.$router.push('/home')
+        },
+    }
+</script>
